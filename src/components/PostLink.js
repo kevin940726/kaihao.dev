2823e7741e661d60635a3005f175258da6143a1a
Kai Hao <kevin830726@gmail.com>
2019-04-19 19:02:56 +0800

    Open new tab when clicking on post link

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -1,14 +1,24 @@
-import styled from '@emotion/styled';
+import React from 'react';
+import { css } from '@emotion/core';
 import { MAIN_TEXT, SUB_TEXT } from '../constants';
 
-const PostLink = styled.a`
-  border-bottom: 1px dashed ${MAIN_TEXT};
-  color: ${SUB_TEXT};
+const PostLink = ({ children, ...props }) => (
+  <a
+    css={css`
+      border-bottom: 1px dashed ${MAIN_TEXT};
+      color: ${SUB_TEXT};
 
-  &:hover {
-    border-bottom-style: solid;
-    text-decoration: none;
-  }
-`
+      &:hover {
+        border-bottom-style: solid;
+        text-decoration: none;
+      }
+    `}
+    target="_blank"
+    rel="noopener noreferrer"
+    {...props}
+  >
+    {children}
+  </a>
+);
 
 export default PostLink;
