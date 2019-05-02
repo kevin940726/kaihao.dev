130244dd41b34a2fa38b153d784fcc47c617e4cb
Kai Hao <kevin830726@gmail.com>
2019-05-02 10:38:01 +0800

    Require gatsby's babel-loader in cosmos instead

--- configs/webpack.cosmos.config.js
+++ configs/webpack.cosmos.config.js
@@ -1,13 +1,7 @@
 module.exports = baseConfig => {
-  // Setup js rule to use the locally installed babel-loader and presets/plugins
+  // Setup js rule to use the gatsby version to support graphql calls
   const jsRule = baseConfig.module.rules.find(rule => rule.test.test('.js'));
-  jsRule.exclude = /node_modules\/(?!(gatsby)\/)/;
-  jsRule.use.loader = require.resolve('babel-loader');
-  jsRule.use.options.presets = [require.resolve('babel-preset-gatsby')];
-  jsRule.use.options.plugins = [
-    require.resolve('babel-plugin-remove-graphql-queries'),
-    // require.resolve('@emotion/babel-preset-css-prop'),
-  ];
+  jsRule.use.loader = require.resolve('gatsby/dist/utils/babel-loader.js');
 
   // Remove JSON rule since webpack4 already handles it
   const jsonRuleIndex = baseConfig.module.rules.findIndex(rule =>
