17028a81f8bfbb94e916516e03017ed60509e034
Kai Hao <kevin830726@gmail.com>
2019-05-03 00:31:25 +0800

    Setup global variables for gatsyby-link to work

--- configs/webpack.cosmos.config.js
+++ configs/webpack.cosmos.config.js
@@ -1,6 +1,9 @@
+const webpack = require('webpack');
+
 module.exports = baseConfig => {
   // Setup js rule to use the gatsby version to support graphql calls
   const jsRule = baseConfig.module.rules.find(rule => rule.test.test('.js'));
+  jsRule.exclude = /node_modules\/(?!gatsby)/;
   jsRule.use.loader = require.resolve('gatsby/dist/utils/babel-loader.js');
 
   // Remove JSON rule since webpack4 already handles it
@@ -22,6 +25,18 @@ module.exports = baseConfig => {
       },
     ],
   });
+
+  // For gatsby-link, see https://github.com/gatsbyjs/gatsby/blob/master/docs/docs/visual-testing-with-storybook.md
+  baseConfig.plugins.push(
+    new webpack.DefinePlugin({
+      __PATH_PREFIX__: JSON.stringify(''),
+      ___loader: {
+        enqueue: () => {},
+        hovering: () => {},
+      },
+      ['window.___navigate']: () => {},
+    })
+  );
 
   // Override mainFields to better match gatsby resolution
   baseConfig.resolve.mainFields = ['browser', 'module', 'main'];
