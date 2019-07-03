4130a3087840c966eb5b2ed7b25e4a06299c3d40
Kai Hao <kevin830726@gmail.com>
2019-07-03 15:13:51 +0800

    Fix CopyButton fixture

--- src/components/__fixtures__/CopyButton.fixture.js
+++ src/components/__fixtures__/CopyButton.fixture.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { css } from '@emotion/core';
 import CopyButton from '../CopyButton';
 
 export default (
   <CopyButton
     code="I'm Mr. Meeseeks!"
-    css={css`
-      transform: translateY(0%);
-      opacity: 1;
-    `}
+    style={{
+      transform: 'translateY(0%)',
+      opacity: '1',
+    }}
   />
 );
