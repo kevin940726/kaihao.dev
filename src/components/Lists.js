7b977bc1bf837030cbc184e0409b54c252113350
Kai Hao <kevin830726@gmail.com>
2019-09-02 18:04:41 +0800

    Adjust Lists margin

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,10 +2,12 @@ import styled from '@emotion/styled';
 
 export const Ul = styled.ul`
   padding-left: 20px;
+  margin: 0 0 2em;
 `;
 
 export const Ol = styled.ol`
   padding-left: 20px;
+  margin: 0 0 2em;
 `;
 
 export const Li = styled.li`
