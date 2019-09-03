e20ec496af7a3a83f2776f2fe5e24e3f21324882
Kai Hao <kevin830726@gmail.com>
2019-09-03 10:28:04 +0800

    Fix nested <ul>, <ol> margin

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -3,11 +3,19 @@ import styled from '@emotion/styled';
 export const Ul = styled.ul`
   padding-left: 20px;
   margin: 0 0 2em;
+
+  & & {
+    margin: 0;
+  }
 `;
 
 export const Ol = styled.ol`
   padding-left: 20px;
   margin: 0 0 2em;
+
+  & & {
+    margin: 0;
+  }
 `;
 
 export const Li = styled.li`
