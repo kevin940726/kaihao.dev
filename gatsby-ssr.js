db90ed816727ed763f82b2e959c798f7078aaf3f
Kai Hao <kevin830726@gmail.com>
2019-09-24 21:58:13 +0800

    Change __dirname to process.cwd()

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,16 +3,18 @@ import path from 'path';
 import React from 'react';
 import Root from './src/components/Root';
 
+const themeModeScript = fs.readFileSync(
+  path.resolve(process.cwd(), './scripts/themeMode.js'),
+  'utf8'
+);
+
 export const wrapRootElement = ({ element }) => <Root>{element}</Root>;
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
   setPreBodyComponents([
     <script
       dangerouslySetInnerHTML={{
-        __html: fs.readFileSync(
-          path.resolve(__dirname, './scripts/themeMode.js'),
-          'utf8'
-        ),
+        __html: themeModeScript,
       }}
     />,
   ]);
